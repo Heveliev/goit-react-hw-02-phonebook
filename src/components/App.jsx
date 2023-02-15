8b1b3e516894e0df7fb7fa03bf541dfd55c5a6e8
Heveliev-destress <tolik05.03@ukr.net>
2023-02-15 15:39:32 +0200

    add input name and number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import { Section } from './Section/Section';
+import {InputBox} from './InputBox/InputBox'
+
+
+
+class App extends React.Component{
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  }
+
+
+  handleInputChange = evt => {
+    let target = evt.currentTarget;
+    console.log(target.name)
+    this.setState(prevState=>{console.log([target.name]) 
+    return {[target.name]: prevState[target.value] }}
+
+    )
+    
+    // this.setState({})
+  }
+
+
+  render(){
+    return (<>
+    <Section title='Phonebook' >
+      <InputBox       
+            value={this.state}
+            onChange={this.handleInputChange}/>
+    </Section>
+      <Section title='Contacts' >
+        
+      </Section>
+      </>
+        );
+  }
+
 };
+
+
+export {App}
